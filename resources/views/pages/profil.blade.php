c9bac22b48ce7b3282d4b3bd167da79b776cdcb1
barraud-samuel <samuel.barraud@hetic.net>
2018-07-17 11:23:33 +0200

    add info in the profil page

--- resources/views/pages/profil.blade.php
+++ resources/views/pages/profil.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.app')
 @section('content')
-    <p>profil</p>
+    <h2 class="text-center">profil</h2>
+    <div class="card">
+        <div class="card-header">
+            <p>
+                {{Auth::user()->name}}
+            </p>
+        </div>
+        <div class="card-body">
+            <p>
+                {{Auth::user()->email}}
+            </p>
+        </div>
+    </div>
 @endsection
