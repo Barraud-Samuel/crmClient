130215d9518663933ef05032baec3f7f9a02f820
barraud-samuel <samuel.barraud@hetic.net>
2018-08-16 17:10:36 +0200

    debug opetration list

--- resources/views/operations/index.blade.php
+++ resources/views/operations/index.blade.php
@@ -29,6 +29,8 @@
                     <th scope="row">{{$indexkey + 1}}</th>
                     <th>
                         @foreach($clients as $key => $client)
+                            <p>{{$client->clientName}}</p>
+                            <p>{{$operation->client_id}}</p>
                             @if($key+1 === $operation->client_id)
                             <a href="/clients/{{$operation->client_id}}">{{$client->clientName}}</a>
                             @endif
