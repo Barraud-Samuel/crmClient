f0fe8d63171f9375827970ec2f61f0610847feea
barraud-samuel <samuel.barraud@hetic.net>
2018-08-17 17:01:26 +0200

    ofihdsf

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,6 +3,6 @@
     <div class="jumbotron text-center">
         <h1>Bonjour</h1>
         <h2>Bienvenue sur le gestionnaire de client Pronogoal</h2>
-        <p><a class="btn btn-primary btn-lg" href="/clients" role="button">Voir les clients</a> <a class="btn btn-success btn-lg" href="/clients/create" role="button">Creer un client</a></p>
+        <p><a class="btn btn-primary btn-lg" href="/clients" role="button">Voir les clients</a> <a class="btn btn-success btn-lg" href="/operations" role="button">Voir les Opérations</a></p>
     </div>
 @endsection
